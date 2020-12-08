7a64f2f247421f77c8363329d7201ce895bc27f6
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-12-08 10:01:33 +0700

    create admin login and create flash message function

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,11 +1,28 @@
 <?php 
 
 class Home extends CI_Controller {
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model("Sess","sess");
+	}
+
 	public function index()
 	{
-		$data['pagetitle'] = "Home";
-		$this->load->view("zzz/head",$data);
-		$this->load->view("home/home");
-		$this->load->view("zzz/foot");
+		if ( isset($_POST['adminlogin']) ) {
+			$data['password'] = $this->input->post("password",true);
+			$login = $this->sess->admin_set($data);
+			if ( $login ) {
+				redirect(base_url("admin"));
+			} else {
+				$this->sess->set_flash("Wrong password!","error");
+				redirect(base_url());
+			}
+		} else {
+			$data['pagetitle'] = "Home";
+			$this->load->view("zzz/head",$data);
+			$this->load->view("home/home");
+			$this->load->view("zzz/foot");
+		}
 	}
 }
---
+++ application/models/Sess.php
@@ -0,0 +1,23 @@
+<?php 
+
+class Sess extends CI_Model {
+	public function set_flash($msg,$status)
+	{
+		$_SESSION["flash_data"] = [
+			"msg" => $msg,
+			"status" => $status
+		];
+		return true;
+	}
+
+	public function admin_set($data)
+	{
+		$password = $data['password'];
+		if ( $password == "123" ) {
+			$_SESSION["admin"] = true;
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
