1971ce929d9b7cc5f7b12a327f6fa7995bf504e8
dimasfirmansyxx <d9firmansyah@gmail.com>
2020-12-10 13:37:38 +0700

    create exit button function

--- application/views/home/home.php
+++ application/views/home/home.php
@@ -7,7 +7,7 @@
         <div class="menu-button">
           <a href="#" class="btn-start"><img src="<?= base_url() ?>assets/template/img/btn-start.png" class="img-fluid mb-4"></a>
           <a href="#"><img src="<?= base_url() ?>assets/template/img/btn-leaderboard.png" class="img-fluid mb-4"></a>
-          <a href="#"><img src="<?= base_url() ?>assets/template/img/btn-exit.png" class="img-fluid mb-4"></a>
+          <a href="#" class="btn-exit"><img src="<?= base_url() ?>assets/template/img/btn-exit.png" class="img-fluid mb-4"></a>
         </div>
     </div>
 </div>
@@ -46,6 +46,18 @@
   </div>
 </div>
 
+<div class="modal fade" id="mdlExit" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-body text-center p-4">
+        <h2>Are you sure want to exit ?</h2>
+        <button type="button" class="btn btn-secondary mt-3 mr-1 btn-lg" data-dismiss="modal">No</button>
+        <a href="#" class="btn btn-lg text-white mt-3 ml-1 btn-confirm-exit" style="background: #700bfb">Yes</a>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script>
     $(function(){
         $(".btn-start").on("click",function(){
@@ -62,6 +74,14 @@
                 <?php endif ?>
              }       
         })  
+
+        $(".btn-exit").on("click",function(){
+          $("#mdlExit").modal("show")
+        })
+
+        $(".btn-confirm-exit").on("click",function(){
+          open(location, '_self').close();
+        })
     })
 </script>
 
